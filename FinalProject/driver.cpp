1af1d42758baadb9aa810194bb545774f83c2141
Bu Sun Kim <busunkim96@gmail.com>
2015-04-20 19:10:55 -0600

    added class and driver files

---
+++ FinalProject/driver.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "courses.h"
+using namespace std;
+
+int main()
+{
+    courses test("courseList.csv");
+    return 0;
+}
---
+++ FinalProject/include/courses.h
@@ -0,0 +1,19 @@
+#ifndef COURSES_H
+#define COURSES_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+class courses
+{
+    public:
+        courses(std::string csvFileName);
+        virtual ~courses();
+    protected:
+    private:
+
+};
+
+#endif // COURSES_H
